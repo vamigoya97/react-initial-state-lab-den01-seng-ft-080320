7458ca465089d82ffaff7d489184affb59a00158
vamigoya97 <amigo.victor.97@gmail.com>
2020-10-12 20:06:27 -0400

    lab completed

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,20 @@
 // your Bomb code here!
+import React from 'react';
+
+export default class Bomb extends React.Component {
+
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+    }
+
+    render(){
+        return(
+            <p>
+                {(this.state.secondsLeft === 0 ? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`)}
+            </p>
+        )
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,20 @@
 // your ImageSlider code here!
+import React from 'react';
+
+export default class ImageSlider extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    render() {
+        return (
+            <p>
+                I am on slide {this.state.currentSlideIndex}
+            </p>
+        )
+    }
+}
